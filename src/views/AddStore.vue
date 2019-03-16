86c719edb9dea3b38a7852ca6389c41fc7f77f07
elipmoc <relipmoc101@gmail.com>
2019-03-16 21:33:27 +0900

    店舗登録をリンク張ってちょっと実装もした

--- src/views/AddStore.vue
+++ src/views/AddStore.vue
@@ -1,20 +1,17 @@
 <template>
     <div>
         <h1>店舗追加</h1>
-        <div v-for="store in stores">
-            <div class="form-group">
-                <label for="searchStore">店名</label>
-                <div class="col-sm-10">
-                    <input id="searchStore" class="form-control" type="text" v-model="searchStore" required/>
-                </div>
+        <div class="form-group">
+            <label for="searchStore">店名</label>
+            <div class="col-sm-10">
+                <input id="searchStore" class="form-control" type="text" v-model="searchStore" required/>
+                <button @click="OnButtonClick">検索</button>
             </div>
+        </div>
 
-            <div>
-                <div v-for="store in stores">
-                    <div class="card-body">
-                        <p class="user-info">ユーザ名：{{store.name}}　level：{{store.address}}</p>
-                    </div>
-                </div>
+        <div>
+            <div v-for="store in stores">
+                <store-vue :store="store" @addStore="OnAddStore(store.id)"/>
             </div>
         </div>
     </div>
@@ -24,26 +21,21 @@
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
 import Store from '@/api/protcol/store/Store';
+import StoreVue from '@/components/AddStore/Store';
+import {ApiClient} from '@/api/ApiClient';
 
-@Component
+@Component({components: {StoreVue}})
 export default class AddStore extends Vue {
     private stores: Store[] = [];
-    private dialog = false;
     private searchStore = '';
 
-    private created() {
-        // ここにさーばから取得したデータ入れる。
-        this.stores.push({storeId: 0, storeName: 'hoge aho店', storeAddress: 'hoge県hoge市huge町'});
+    private async OnButtonClick() {
+        this.stores = await new ApiClient().FindStoreName(this.searchStore);
     }
 
-    private On_ButtonClick() {
-        this.dialog = true;
+    private OnAddStore(id: number) {
+        alert(id);
     }
-
-    private Close() {
-        this.dialog = false;
-    }
-
 }
 </script>
 
