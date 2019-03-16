2f3bd06eb4b68d21b3a1bc5da9aaca5ad5e3d466
hiroki19990625 <hiroki19990625@gmail.com>
2019-03-16 18:56:12 +0900

    ApiClientにLoginUserを追加

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -4,23 +4,24 @@ import {UserSex} from "../api/protcol/user/UserSex";
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
-import {ApiClient} from '@/api/ApiClient';
-import {UserSex} from '@/api/protcol/user/UserSex';
+    import {Component, Vue} from 'vue-property-decorator';
+    import {ApiClient} from '@/api/ApiClient';
+    import {UserSex} from '@/api/protcol/user/UserSex';
 
-@Component
-export default class TestPage extends Vue {
-    public async created() {
-        const api = new ApiClient();
-        // console.log(api.GetPrivateUser('hoge'));
-        // console.log(await api.CreateUser({
-        //     user_name: "hoge_san",
-        //     sex: UserSex.Men,
-        //     pass: "abcdefg1234",
-        //     birthday: "1980-01-01"
-        // }));
+    @Component
+    export default class TestPage extends Vue {
+        public async created() {
+            const api = new ApiClient();
+            // console.log(api.GetPrivateUser('hoge'));
+            // console.log(await api.CreateUser({
+            //     user_name: "hoge_san",
+            //     sex: UserSex.Men,
+            //     pass: "abcdefg1234",
+            //     birthday: "1980-01-01"
+            // }));
+            // console.log(await api.LoginUser("abcdefg1234", "hoge_san"));
+        }
     }
-}
 </script>
 
 <style scoped>
