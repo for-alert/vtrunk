5c8031fe6ac4d5ebb95318bd2b9baf16bd2fc5f5
Kai4510471 <aria7716@gmail.com>
2019-03-16 16:35:07 +0900

    店舗追加ページ追加

--- src/views/AddStore.vue
+++ src/views/AddStore.vue
@@ -4,15 +4,6 @@
         <div v-for="store in stores">
             <div class="store-body">
                 <a @click="On_ButtonClick" class="store-info">店名：{{store.storeName}}　住所：{{store.storeAddress}}</a>
-                <dialog v-model="dialog" max-width="500px">
-                    <v-card>
-                        <span>店舗確認</span>
-                        <p>店舗に間違いありませんか？</p>
-
-                        <v-spacer></v-spacer>
-                        <v-btn color="blue darken-1" flat @click="close">キャンセル</v-btn>
-                    </v-card>
-                </dialog>
             </div>
         </div>
     </div>
@@ -20,33 +11,33 @@
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
+    import {Component, Vue} from 'vue-property-decorator';
 
-interface Stores {
-    storeId: number;
-    storeName: string;
-    storeAddress: string;
-}
-
-@Component
-export default class AddStore extends Vue {
-    private stores: Stores[] = [];
-    private dialog = false;
-
-    private created() {
-        // ここにさーばから取得したデータ入れる。
-        this.stores.push({storeId: 0, storeName: 'hoge aho店', storeAddress: 'hoge県hoge市huge町'});
+    interface Stores {
+        storeId: number;
+        storeName: string;
+        storeAddress: string;
     }
 
-    private On_ButtonClick() {
-        this.dialog = true;
-    }
+    @Component
+    export default class AddStore extends Vue {
+        private stores: Stores[] = [];
+        private dialog = false;
 
-    private Close() {
-        this.dialog = false;
-    }
+        private created() {
+            // ここにさーばから取得したデータ入れる。
+            this.stores.push({storeId: 0, storeName: 'hoge aho店', storeAddress: 'hoge県hoge市huge町'});
+        }
 
-}
+        private On_ButtonClick() {
+            this.dialog = true;
+        }
+
+        private Close() {
+            this.dialog = false;
+        }
+
+    }
 </script>
 
 <style scoped>
