78a23f0776433e2553ecf7a559f7bb2198311831
hiroki19990625 <hiroki19990625@gmail.com>
2019-03-17 06:38:08 +0900

    ランダムユーザー戦闘の実装

--- src/router.ts
+++ src/router.ts
@@ -8,6 +8,7 @@ import TestPage from '@/views/TestPage.vue';
 import Register from '@/views/Register.vue';
 import Login from '@/views/Login.vue';
 import NotLogin from '@/views/NotLogin.vue';
+import Battle from '@/views/Battle.vue';
 
 Vue.use(Router);
 
@@ -52,6 +53,11 @@ export default new Router({
             path: '/notlogin',
             name: 'notlogin',
             component: NotLogin,
+        },
+        {
+            path: '/battle',
+            name: 'battle',
+            component: Battle,
         },
     ],
 });
