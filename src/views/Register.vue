ff79d6d1186b8d9e18b680e488cbaa4abcc101ef
Kai4510471 <aria7716@gmail.com>
2019-03-17 07:11:07 +0900

    登録修正

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -12,7 +12,21 @@ import {UserSex} from "../api/protcol/user/UserSex";
                         item-text="state"
                         item-value="abbr"
                         label="性別"></v-select>
-                <v-text-field v-model="birthday" label="誕生日"></v-text-field>
+                <v-select
+                        v-model="year"
+                        :items="years"
+                        :item-value="years"
+                        label="誕生日 年"></v-select>
+                <v-select
+                        v-model="month"
+                        :items="monthes"
+                        :item-value="monthes"
+                        label="誕生日　月"></v-select>
+                <v-select
+                        v-model="day"
+                        :items="days"
+                        :item-value="days"
+                        label="誕生日　日"></v-select>
                 <v-text-field type="password" v-model="password" label="パスワード"></v-text-field>
                 <v-text-field type="password" v-model="rePassword" label="パスワード再入力"></v-text-field>
                 <v-alert color="error" icon="warning" outline v-show="message">{{message}}</v-alert>
@@ -24,65 +38,96 @@ import {UserSex} from "../api/protcol/user/UserSex";
 </template>
 
 <script lang="ts">
-import {Component, Vue} from 'vue-property-decorator';
-import {ApiClient} from '@/api/ApiClient';
-import {UserSex} from '@/api/protcol/user/UserSex';
+    import {Component, Vue} from 'vue-property-decorator';
+    import {ApiClient} from '@/api/ApiClient';
+    import {UserSex} from '@/api/protcol/user/UserSex';
 
-@Component
-export default class Register extends Vue {
-    private userName = '';
-    private sex: UserSex | null = null;
-    private birthday = '';
-    private password = '';
-    private rePassword = '';
-    private message = '';
-    private items = [
-        {state: '男', abbr: 'man'},
-        {state: '女', abbr: 'woman'},
-        {state: '非公開', abbr: 'private'},
-    ];
+    @Component
+    export default class Register extends Vue {
+        private userName = '';
+        private sex: UserSex | null = null;
+        private birthday = '';
+        private password = '';
+        private rePassword = '';
+        private message = '';
+        private items = [
+            {state: '男', abbr: 'man'},
+            {state: '女', abbr: 'woman'},
+            {state: '非公開', abbr: 'private'},
+        ];
+        private years: string[] = [];
+        private monthes: string[] = [];
+        private days: string[] = [];
+        private day = '';
+        private year = '';
+        private month = '';
+        private i = 1;
 
+        private created() {
+            for (this.i; this.i <= 31; this.i++) {
+                if (this.i <= 9) {
+                    this.days.push('0' + this.i.toString());
+                } else {
+                    this.days.push(this.i.toString());
+                }
+            }
+            this.i = 1;
+            for (this.i; this.i <= 12; this.i++) {
+                if (this.i <= 9) {
+                    this.monthes.push('0' + this.i.toString());
+                } else {
+                    this.monthes.push(this.i.toString());
+                }
+            }
+            this.i = 0;
+            for (this.i; this.i <= 100; this.i++) {
+                this.years.push((1950 + this.i).toString());
+            }
 
-    private OnLogin() {
-        this.$router.push('/login');
+        }
+
+        private OnLogin() {
+            this.$router.push('/login');
+        }
+
+        private async OnClick_register() {
+            this.birthday = this.year + '-' + this.month + '-' + this.day;
+
+            if (!this.userName) {
+                this.message = 'ユーザ名が入力されていません。';
+                return;
+            }
+            if (!this.sex) {
+                this.message = '性別が選択されていません。';
+                return;
+            }
+
+            if (!this.birthday) {
+                this.message = '誕生日が入力されていません。';
+                return;
+            }
+            if (!this.password) {
+                this.message = 'パスワードが入力されていません。';
+                return;
+            }
+            if (this.password !== this.rePassword) {
+                this.message = 'パスワードが一致しません。';
+                return;
+            }
+            try {
+                const token = await new ApiClient().CreateUser({
+                    user_name: this.userName,
+                    sex: this.sex,
+                    pass: this.password,
+                    birthday: this.birthday,
+                });
+                this.$cookies.set('user_token', token);
+                this.$router.push('/mypage');
+            } catch (e) {
+                alert(e);
+            }
+        }
     }
-
-    private async OnClick_register() {
-        if (!this.userName) {
-            this.message = 'ユーザ名が入力されていません。';
-            return;
-        }
-        if (!this.sex) {
-            this.message = '性別が選択されていません。';
-            return;
-        }
-
-        if (!this.birthday) {
-            this.message = '誕生日が入力されていません。';
-            return;
-        }
-        if (!this.password) {
-            this.message = 'パスワードが入力されていません。';
-            return;
-        }
-        if (this.password !== this.rePassword) {
-            this.message = 'パスワードが一致しません。';
-            return;
-        }
-        try {
-            const token = await new ApiClient().CreateUser({
-                user_name: this.userName,
-                sex: this.sex,
-                pass: this.password,
-                birthday: this.birthday,
-            });
-            this.$cookies.set('user_token', token);
-            this.$router.push('/mypage');
-        } catch (e) {
-            alert(e);
-        }
-    }
-}
 </script>
 
 <style scoped>
