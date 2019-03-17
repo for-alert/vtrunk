50a5a1ddb2e046a9812e988f15f29f61e336f1f4
Kai4510471 <aria7716@gmail.com>
2019-03-17 13:37:35 +0900

    battle

--- src/views/Battle.vue
+++ src/views/Battle.vue
@@ -46,9 +46,15 @@ import {BattleState} from "./BattleState";
             <v-card>
                 <v-card-text>
                     <p>{{message}}</p>
+                    <p>{{lvmessage}}</p>
                 </v-card-text>
                 <v-card-actions>
-                    <v-btn @click="OnClose">OK</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn @click="OnClose"
+                           color="green darken-1"
+                           flat="flat">
+                        OK
+                    </v-btn>
                 </v-card-actions>
             </v-card>
         </v-dialog>
@@ -56,16 +62,32 @@ import {BattleState} from "./BattleState";
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from 'vue-property-decorator';
-    import {PublicUser} from '../api/protcol/user/PublicUser';
-    import {ApiClient} from '../api/ApiClient';
-    import {RandomBattle} from '../api/protcol/battle/RandomBattle';
-    import {BattleState} from '@/views/BattleState';
-    import {UserSex} from '@/api/protcol/user/UserSex';
+import {Component, Vue} from 'vue-property-decorator';
+import {PublicUser} from '../api/protcol/user/PublicUser';
+import {ApiClient} from '../api/ApiClient';
+import {RandomBattle} from '../api/protcol/battle/RandomBattle';
+import {BattleState} from '@/views/BattleState';
+import {UserSex} from '@/api/protcol/user/UserSex';
 
-    @Component
-    export default class Battle extends Vue {
-        private myUser: PublicUser = {
+@Component
+export default class Battle extends Vue {
+    private myUser: PublicUser = {
+        userId: 0,
+        userName: '',
+        sex: UserSex.Men,
+        birthday: '',
+        nextExp: 0,
+        exp: 0,
+        level: 0,
+    };
+    private battleResult: RandomBattle = {
+        win: false,
+        fix: 0,
+        levelUp: {
+            getExp: 0,
+            levelUp: false,
+        },
+        battleUser: {
             userId: 0,
             userName: '',
             sex: UserSex.Men,
@@ -73,103 +95,93 @@ import {BattleState} from "./BattleState";
             nextExp: 0,
             exp: 0,
             level: 0,
-        };
-        private battleResult: RandomBattle = {
-            win: false,
-            fix: 0,
-            levelUp: null,
-            battleUser: {
-                userId: 0,
-                userName: '',
-                sex: UserSex.Men,
-                birthday: '',
-                nextExp: 0,
-                exp: 0,
-                level: 0,
-            },
-        };
+        },
+    };
 
-        private state: BattleState = BattleState.Wait;
+    private state: BattleState = BattleState.Wait;
 
-        private timer: number = 0;
-        private countDown: number = 0;
+    private timer: number = 0;
+    private countDown: number = 0;
 
-        private moveNumber: number = 0;
+    private moveNumber: number = 0;
 
-        private dialog: boolean = false;
-        private message: string = '';
+    private dialog: boolean = false;
+    private message: string = '';
+    private lvmessage = '';
 
-        public get Dialog() {
-            return this.dialog;
+    public get Dialog() {
+        return this.dialog;
+    }
+
+    public set Dialog(v: boolean) {
+        if (!v) {
+            this.OnClose();
         }
 
-        public set Dialog(v: boolean) {
-            if (!v) {
-                this.OnClose();
-            }
+        this.dialog = v;
+    }
 
-            this.dialog = v;
+    private async created() {
+        const WAIT_COUNT_DOWN_TIME = 2000;
+        const token = this.$cookies.get('user_token');
+        if (token) {
+            const api = new ApiClient();
+            this.myUser = await api.GetPublicUserByToken(token);
+            this.battleResult = await api.RandomBattle(token);
+            this.countDown = WAIT_COUNT_DOWN_TIME;
+        } else {
+            this.$router.push('/');
+            return;
         }
 
-        private async created() {
-            const WAIT_COUNT_DOWN_TIME = 2000;
-            const token = this.$cookies.get('user_token');
-            if (token) {
-                const api = new ApiClient();
-                this.myUser = await api.GetPublicUserByToken(token);
-                this.battleResult = await api.RandomBattle(token);
-                this.countDown = WAIT_COUNT_DOWN_TIME;
-            } else {
-                this.$router.push('/');
+        this.Wait();
+    }
+
+    private Wait() {
+        this.timer = setInterval(() => {
+            if (this.countDown <= 0) {
+                this.state = BattleState.Last_Wait;
+                clearInterval(this.timer);
+                this.LastWait();
                 return;
             }
 
-            this.Wait();
-        }
-
-        private Wait() {
-            this.timer = setInterval(() => {
-                if (this.countDown <= 0) {
-                    this.state = BattleState.Last_Wait;
-                    clearInterval(this.timer);
-                    this.LastWait();
-                    return;
-                }
-
-                this.moveNumber++;
-                this.countDown--;
-            }, 1);
-        }
-
-
-        private LastWait() {
-            const START_COUNT_DOWN_TIME = 1000;
-            this.countDown = START_COUNT_DOWN_TIME;
-            this.timer = setInterval(() => {
-                if (this.countDown <= 0) {
-                    this.state = BattleState.End;
-                    clearInterval(this.timer);
-                    this.dialog = true;
-                    this.message = this.battleResult.levelUp ?
-                        '経験値を' + this.battleResult.levelUp.getExp + '取得しました。' :
-                        '敗北しました。';
-                    this.message += this.battleResult.levelUp.levelUp ? '\nレベルUP!!' : '';
-                }
-
-                this.countDown--;
-            }, 1);
-        }
-
-        private Cut(val: number) {
-            const c = val * 100;
-            return Math.floor(c) / 100;
-        }
-
-        private OnClose() {
-            this.dialog = false;
-            this.$router.push('/');
-        }
+            this.moveNumber++;
+            this.countDown--;
+        }, 1);
     }
+
+
+    private LastWait() {
+        const START_COUNT_DOWN_TIME = 1000;
+        this.countDown = START_COUNT_DOWN_TIME;
+        this.timer = setInterval(() => {
+            if (this.countDown <= 0) {
+                this.state = BattleState.End;
+                clearInterval(this.timer);
+                this.dialog = true;
+                this.message = this.battleResult.levelUp ?
+                    '経験値を' + this.battleResult.levelUp.getExp + '取得しました。' :
+                    '敗北しました。';
+                if (this.battleResult.levelUp) {
+                    this.lvmessage = this.battleResult.levelUp.levelUp ? '\nレベルUP!!' : '';
+                }
+            }
+
+            this.countDown--;
+        }, 1);
+    }
+
+    private Cut(val: number) {
+        const c = val * 100;
+        return Math.floor(c) / 100;
+    }
+
+    private OnClose() {
+        this.dialog = false;
+        this.$router.push('/');
+    }
+}
 </script>
 
 <style scoped>
