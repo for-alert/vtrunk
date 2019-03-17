77935868877f52245136588404fa217156c647b8
elipmoc <relipmoc101@gmail.com>
2019-03-17 14:20:15 +0900

    マップを毎回作成するように

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -17,17 +17,18 @@ import {Store} from '@/api/protcol/store/Store';
 export default class MapView extends Vue {
     private stores: Store[];
     private pos: any[];
+    private map: Microsoft.Maps.Map | null = null;
 
     public mounted() {
         const token = this.$cookies.get('user_token');
         if (token) {
             const pos = new Microsoft.Maps.Location(this.pos[0].coords.latitude, this.pos[0].coords.longitude);
-            const map = new Microsoft.Maps.Map('#map', {
+            if (this.map) { this.map.dispose(); }
+            this.map = new Microsoft.Maps.Map('#map', {
                 credentials: 'AizjfRpuOsvfOpcPlLzrQtMrdxBxXi8xlbJUrfMv8ibleQ4pAhrQtZLIMwX3iLVj',
                 center: pos,
-                zoom: 10,
             });
-            map.entities.push(this.stores.map((d) => {
+            this.map.entities.push(this.stores.map((d) => {
                 return new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(d.latitude, d.longitude), {color: 'red'});
             }));
         }
